2d16ee6e26ac9ac26634557838cb41116eebf11a
Ishani Amin <ishaniamin@Ishanis-MacBook-Pro.local>
2017-02-21 11:55:31 -0500

    update

--- seeds.sql
+++ seeds.sql
@@ -20,4 +20,4 @@ INSERT INTO products (product, department_id, price, stock_quantity) VALUES ('Be
 INSERT INTO products (product, department_id, price, stock_quantity) VALUES ('Handbags', 3, 15.00, 100);
 INSERT INTO products (product, department_id, price, stock_quantity) VALUES ('Scarf', 3, 15.00, 100);
 
-INSERT INTO sales (product_id, quantity_purchased, created_at) VALUES (, , NOW());
+-- INSERT INTO sales (product_id, quantity_purchased, created_at) VALUES (, , NOW());
